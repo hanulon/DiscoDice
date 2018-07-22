33c87ab6d3a8dcf8dcfce3025f55ad80e0de4346
hanulon <enialis4@gmail.com>
2018-07-22 15:04:17 +0200

    All methods coded, with unit tests.

--- macros_agent.py
+++ macros_agent.py
@@ -1,43 +1,54 @@
+import json
+
 
 class MacrosAgent:
     def __init__(self):
-        self.macros_in_memory = []
+        self.macros_in_memory = {}
 
-    def add_macro(self, commands, alias):
+    def define_macro(self, alias, commands):
         if alias is None or commands is None:
             raise ValueError("Command/alias cannot be None")
         if alias.__len__() == 0 or commands.__len__() == 0:
             raise ValueError("Command/alias cannot be zero-length strings.")
-
-        new_macro = {'alias': alias, 'command': commands}
-
-        if new_macro['alias'] in [element['alias'] for element in self.macros_in_memory]:
+        if alias in self.get_all_macros_aliases():
             raise ValueError("There already exists macro with that alias.")
 
-        self.macros_in_memory.append(new_macro)
+        self.macros_in_memory[alias] = commands
 
     def forget_all_macros(self):
-        self.macros_in_memory = []
+        self.macros_in_memory = {}
+
+    def get_all_macros_aliases(self):
+        return list(self.macros_in_memory.keys())
 
     def forget_macro(self, macro_alias):
-        macro_to_forget = self.find_macro(macro_alias)
-        if macro_to_forget is not None:
-            self.macros_in_memory.remove(macro_to_forget)
-
-    def find_macro(self, macro_alias):
-        list_of_aliases_in_memory = [element['alias'] for element in self.macros_in_memory]
-        if macro_alias not in list_of_aliases_in_memory:
-            return None
-        return self.macros_in_memory[list_of_aliases_in_memory.index(macro_alias)]
+        if macro_alias in self.get_all_macros_aliases():
+            self.macros_in_memory.pop(macro_alias)
+            return True
+        return False
 
     def redefine_macro(self, alias, commands):
-        return None
+        if alias in self.get_all_macros_aliases():
+            self.macros_in_memory[alias] = commands
+            return True
+        return False
 
     def get_macro_command(self, macro_alias):
-        return None
-
-    def load_macros_from_file(self, path):
+        if macro_alias in self.get_all_macros_aliases():
+            return self.macros_in_memory[macro_alias]
         return None
 
     def save_macros_to_file(self, path):
-        return None
+        js = json.dumps(self.macros_in_memory)
+        f_out = open(path, "w")
+        f_out.write(js)
+        f_out.close()
+
+    def load_macros_from_file(self, path):
+        f_in = open(path, "r")
+        js = json.load(f_in)
+        f_in.close()
+        self.macros_in_memory = js
+
+    def get_all_macros(self):
+        return self.macros_in_memory
--- tests/test_macros_agent.py
+++ tests/test_macros_agent.py
@@ -1,79 +1,122 @@
 from unittest import TestCase
 import macros_agent as macros
+import os
 
 
 class TestMacrosAgent(TestCase):
-    def test_add_macro_should_add_element_to_macro_list(self):
+    def test_define_macro_should_add_element_to_macro_list(self):
         agent = macros.MacrosAgent()
         new_macro = ["stamina_roll", "/roll 4d10"]
 
-        agent.add_macro(new_macro[1], new_macro[0])
+        agent.define_macro(new_macro[0], new_macro[1])
 
-        self.assertEqual({'alias': new_macro[0], 'command': new_macro[1]}, agent.macros_in_memory[0])
+        self.assertEqual("/roll 4d10", agent.macros_in_memory["stamina_roll"])
 
-    def test_add_macro_should_raise_exception_on_missing_alias_or_commands(self):
+    def test_define_macro_should_raise_exception_on_missing_alias_or_commands(self):
         agent = macros.MacrosAgent()
         new_macro = ["stamina_roll", "/roll 4d10"]
 
         with self.assertRaises(expected_exception=ValueError):
-            agent.add_macro(None, new_macro[0])
+            agent.define_macro(None, new_macro[0])
         with self.assertRaises(expected_exception=ValueError):
-            agent.add_macro("", new_macro[0])
+            agent.define_macro("", new_macro[0])
         with self.assertRaises(expected_exception=ValueError):
-            agent.add_macro(new_macro[1], "")
+            agent.define_macro(new_macro[1], "")
 
-    def test_add_macro_should_element_be_dictionary(self):
+    def test_define_macro_should_raise_exception_on_duplicating_aliases(self):
         agent = macros.MacrosAgent()
         new_macro = ["stamina_roll", "/roll 4d10"]
 
-        agent.add_macro(new_macro[1], new_macro[0])
-
-        self.assertEqual("stamina_roll", agent.macros_in_memory[0]['alias'])
-        self.assertEqual("/roll 4d10", agent.macros_in_memory[0]['command'])
-
-    def test_add_macro_should_raise_exception_on_duplicating_aliases(self):
-        agent = macros.MacrosAgent()
-        new_macro = ["stamina_roll", "/roll 4d10"]
-
-        agent.add_macro(new_macro[1], new_macro[0])
+        agent.define_macro(new_macro[0], new_macro[1])
 
         with self.assertRaises(expected_exception=ValueError):
-            agent.add_macro("whatever", new_macro[0])
+            agent.define_macro(new_macro[0], "whatever")
 
     def test_forget_all_macros(self):
         agent = macros.MacrosAgent()
         new_macro1 = ["stamina_roll", "/roll 4d10"]
         new_macro2 = ["stamina_rolls", "/roll 4d10"]
 
-        agent.add_macro(new_macro1[1], new_macro1[0])
-        agent.add_macro(new_macro2[1], new_macro2[0])
+        agent.define_macro(new_macro1[0], new_macro1[1])
+        agent.define_macro(new_macro2[0], new_macro2[1])
         size_pre_purge = agent.macros_in_memory.__len__()
         agent.forget_all_macros()
 
         self.assertEqual(2, size_pre_purge)
-        self.assertEqual([], agent.macros_in_memory)
+        self.assertEqual({}, agent.macros_in_memory)
 
     def test_forget_macro(self):
         agent = macros.MacrosAgent()
         new_macro1 = ["stamina_roll", "/roll 4d10"]
-        new_macro2 = ["strength_roll", "/roll 4d10"]
+        new_macro2 = ["strength_roll", "/roll 4d104"]
         macro_alias_to_forget = "stamina_roll"
 
-        agent.add_macro(new_macro1[1], new_macro1[0])
-        agent.add_macro(new_macro2[1], new_macro2[0])
-        agent.forget_macro(macro_alias_to_forget)
+        agent.define_macro(new_macro1[0], new_macro1[1])
+        agent.define_macro(new_macro2[0], new_macro2[1])
+        forgot_macro = agent.forget_macro(macro_alias_to_forget)
 
         self.assertEqual(1, agent.macros_in_memory.__len__())
-        self.assertEqual("strength_roll", agent.macros_in_memory[0]['alias'])
+        self.assertTrue(forgot_macro)
+        with self.assertRaises(expected_exception=KeyError):
+            agent.macros_in_memory[macro_alias_to_forget]
 
-    def test_find_macro(self):
+    def test_redefine_macro(self):
         agent = macros.MacrosAgent()
         new_macro1 = ["stamina_roll", "/roll 4d10"]
-        new_macro2 = ["strength_roll", "/roll 4d10"]
-        macro_alias_to_find = "strength_roll"
+        new_macro2 = ["stamina_roll", "/roll 4d104"]
 
-        agent.add_macro(new_macro1[1], new_macro1[0])
-        agent.add_macro(new_macro2[1], new_macro2[0])
-        macro = agent.find_macro(macro_alias_to_find)
+        agent.define_macro(new_macro1[0], new_macro1[1])
+        is_redefined = agent.redefine_macro(new_macro2[0], new_macro2[1])
 
-        self.assertEqual({'alias': "strength_roll", 'command': "/roll 4d10"}, macro)
+        self.assertTrue(is_redefined)
+        self.assertEqual("/roll 4d104", agent.macros_in_memory["stamina_roll"])
+
+    def test_get_macro_command(self):
+        agent = macros.MacrosAgent()
+        new_macro = ["stamina_roll", "/roll 4d10"]
+
+        agent.define_macro(new_macro[0], new_macro[1])
+        command = agent.get_macro_command(new_macro[0])
+
+        self.assertEqual("/roll 4d10", command)
+
+    def test_get_macro_command_should_return_none_if_not_found(self):
+        agent = macros.MacrosAgent()
+        new_macro = ["stamina_roll", "/roll 4d10"]
+
+        command = agent.get_macro_command(new_macro[0])
+
+        self.assertEqual(None, command)
+
+    def test_save_macros_to_file(self):
+        agent = macros.MacrosAgent()
+        new_macro1 = ["stamina_roll", "/roll 4d10"]
+        new_macro2 = ["sss", "/roll 10d4"]
+        filename = "test_file.json"
+
+        agent.define_macro(new_macro1[0], new_macro1[1])
+        agent.define_macro(new_macro2[0], new_macro2[1])
+        agent.save_macros_to_file(filename)
+
+        f = open(filename, "r")
+        result = f.readline()
+        f.close()
+        os.remove(filename)
+
+        self.assertEqual('{"stamina_roll": "/roll 4d10", "sss": "/roll 10d4"}', result)
+
+    def test_load_macros_from_file(self):
+        agent = macros.MacrosAgent()
+        new_macro1 = ["stamina_roll", "/roll 4d10"]
+        new_macro2 = ["sss", "/roll 10d4"]
+        filename = "test_file.json"
+
+        agent.define_macro(new_macro1[0], new_macro1[1])
+        agent.define_macro(new_macro2[0], new_macro2[1])
+        expected_dict = agent.macros_in_memory
+
+        agent.save_macros_to_file(filename)
+        agent.load_macros_from_file(filename)
+        os.remove(filename)
+
+        self.assertEqual(expected_dict, agent.macros_in_memory)
