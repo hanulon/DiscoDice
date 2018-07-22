809311a7434a67f73288c8d735a0fe5f4a8b0ae5
hanulon <enialis4@gmail.com>
2018-07-22 13:22:41 +0200

    Added new methods to MacrosAgent, prepared empty functions for future implementation

--- macros_agent.py
+++ macros_agent.py
@@ -1,7 +1,6 @@
 
 class MacrosAgent:
     def __init__(self):
-        self.macros_store_filename = 'macros_store.txt'
         self.macros_in_memory = []
 
     def add_macro(self, commands, alias):
@@ -16,3 +15,29 @@ class MacrosAgent:
             raise ValueError("There already exists macro with that alias.")
 
         self.macros_in_memory.append(new_macro)
+
+    def forget_all_macros(self):
+        self.macros_in_memory = []
+
+    def forget_macro(self, macro_alias):
+        macro_to_forget = self.find_macro(macro_alias)
+        if macro_to_forget is not None:
+            self.macros_in_memory.remove(macro_to_forget)
+
+    def find_macro(self, macro_alias):
+        list_of_aliases_in_memory = [element['alias'] for element in self.macros_in_memory]
+        if macro_alias not in list_of_aliases_in_memory:
+            return None
+        return self.macros_in_memory[list_of_aliases_in_memory.index(macro_alias)]
+
+    def redefine_macro(self, alias, commands):
+        return None
+
+    def get_macro_command(self, macro_alias):
+        return None
+
+    def load_macros_from_file(self, path):
+        return None
+
+    def save_macros_to_file(self, path):
+        return None
--- tests/test_macros_agent.py
+++ tests/test_macros_agent.py
@@ -9,7 +9,7 @@ class TestMacrosAgent(TestCase):
 
         agent.add_macro(new_macro[1], new_macro[0])
 
-        self.assertEqual(agent.macros_in_memory[0], {'alias': new_macro[0], 'command': new_macro[1]})
+        self.assertEqual({'alias': new_macro[0], 'command': new_macro[1]}, agent.macros_in_memory[0])
 
     def test_add_macro_should_raise_exception_on_missing_alias_or_commands(self):
         agent = macros.MacrosAgent()
@@ -28,8 +28,8 @@ class TestMacrosAgent(TestCase):
 
         agent.add_macro(new_macro[1], new_macro[0])
 
-        self.assertEqual(agent.macros_in_memory[0]['alias'], "stamina_roll")
-        self.assertEqual(agent.macros_in_memory[0]['command'], "/roll 4d10")
+        self.assertEqual("stamina_roll", agent.macros_in_memory[0]['alias'])
+        self.assertEqual("/roll 4d10", agent.macros_in_memory[0]['command'])
 
     def test_add_macro_should_raise_exception_on_duplicating_aliases(self):
         agent = macros.MacrosAgent()
@@ -39,3 +39,41 @@ class TestMacrosAgent(TestCase):
 
         with self.assertRaises(expected_exception=ValueError):
             agent.add_macro("whatever", new_macro[0])
+
+    def test_forget_all_macros(self):
+        agent = macros.MacrosAgent()
+        new_macro1 = ["stamina_roll", "/roll 4d10"]
+        new_macro2 = ["stamina_rolls", "/roll 4d10"]
+
+        agent.add_macro(new_macro1[1], new_macro1[0])
+        agent.add_macro(new_macro2[1], new_macro2[0])
+        size_pre_purge = agent.macros_in_memory.__len__()
+        agent.forget_all_macros()
+
+        self.assertEqual(2, size_pre_purge)
+        self.assertEqual([], agent.macros_in_memory)
+
+    def test_forget_macro(self):
+        agent = macros.MacrosAgent()
+        new_macro1 = ["stamina_roll", "/roll 4d10"]
+        new_macro2 = ["strength_roll", "/roll 4d10"]
+        macro_alias_to_forget = "stamina_roll"
+
+        agent.add_macro(new_macro1[1], new_macro1[0])
+        agent.add_macro(new_macro2[1], new_macro2[0])
+        agent.forget_macro(macro_alias_to_forget)
+
+        self.assertEqual(1, agent.macros_in_memory.__len__())
+        self.assertEqual("strength_roll", agent.macros_in_memory[0]['alias'])
+
+    def test_find_macro(self):
+        agent = macros.MacrosAgent()
+        new_macro1 = ["stamina_roll", "/roll 4d10"]
+        new_macro2 = ["strength_roll", "/roll 4d10"]
+        macro_alias_to_find = "strength_roll"
+
+        agent.add_macro(new_macro1[1], new_macro1[0])
+        agent.add_macro(new_macro2[1], new_macro2[0])
+        macro = agent.find_macro(macro_alias_to_find)
+
+        self.assertEqual({'alias': "strength_roll", 'command': "/roll 4d10"}, macro)
