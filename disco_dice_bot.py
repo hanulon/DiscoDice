3da32bfead445e95e20eee0916e99ca3f95a2874
hanulon <enialis4@gmail.com>
2018-07-22 16:23:58 +0200

    DiscoDiceBot class uses now macros. Needs cleaning and improvement.

--- disco_dice_bot.py
+++ disco_dice_bot.py
@@ -1,40 +1,73 @@
 import discord
 from discord.ext import commands
 import dice_roller
+import macros_agent
 
 
-def get_bot_credentials(bot_creds_file):
-    file = open(bot_creds_file, "r")
-    read_lines = [line.strip() for line in file.readlines()]
-    file.close()
-    return read_lines
+class DiscoDiceBot:
+    def __init__(self, bot_credentials_file):
+        self.bot_creds_file = bot_credentials_file
+        self.macros_store_path = "my_macros.json"
+        self.macros_agent = macros_agent.MacrosAgent()
+        self.help_text = "Type: /roll 3d20+3"
 
+    def run(self):
+        Client = discord.Client()
+        client = commands.Bot(command_prefix="?")
 
-def respond_to_command(message):
-    args = message.upper().split(" ")
-    print(args)
-    if args[0] == '/ROLL' or args[0] == '/R':
-        argument = "".join(args[1:])
-        return dice_roller.get_response_to_formula(argument)
-    if args[0] == '/HELP':
-        return "Type: /roll 3d20+3"
-    return None
+        @client.event
+        async def on_ready():
+            print("Dice rolling bot is ready!")
+
+        @client.event
+        async def on_message(message):
+            response = self.respond_to_command(message.content)
+            if response is not None:
+                await client.send_message(message.channel, response)
+
+        client.run(self.get_bot_token())
+
+    def respond_to_command(self, message):
+        command, *args = message.upper().split(" ")
+        print(command, args)
+        if command in ['/ROLL', '/R']:
+            argument = "".join(args)
+            return dice_roller.get_response_to_formula(argument)
+        if command in ['/MACRO', '/MACROS', '/M']:
+            return self.get_macros_command_response(args)
+        if command in ['/HELP', '/H']:
+            return self.help_text
+        return None
+
+    def get_macros_command_response(self, args):
+        if args.__len__() == 0:
+            return self.macros_agent.get_all_macros_aliases()
+        elif args.__len__() == 1:
+            if args[0] == 'CLEAR_ALL':
+                self.macros_agent.forget_all_macros()
+            elif args[0] == 'SAVE':
+                self.macros_agent.save_macros_to_file(self.macros_store_path)
+            elif args[0] == 'LOAD':
+                self.macros_agent.load_macros_from_file(self.macros_store_path)
+            else:
+                return self.macros_agent.get_macro_command(args[0])
+        else:
+            if args[0] == 'DEFINE':
+                self.macros_agent.define_macro(args[1], " ".join(args[2:]))
+                return self.macros_agent.get_all_macros_aliases()
+            if args[0] == 'REDEFINE':
+                self.macros_agent.redefine_macro(args[1], " ".join(args[2:]))
+                return self.macros_agent.get_all_macros_aliases()
+            if args[0] == 'CLEAR':
+                self.macros_agent.forget_macro(args[1])
+
+    def get_bot_token(self):
+        file = open(self.bot_creds_file, "r")
+        read_lines = [line.strip() for line in file.readlines()]
+        file.close()
+        return read_lines[0]
 
 
 if __name__ == "__main__":
-    Client = discord.Client()
-    client = commands.Bot(command_prefix="?")
-    [bot_token, _] = get_bot_credentials("bot_credentials.txt")
-
-    @client.event
-    async def on_ready():
-        print("Dice rolling bot is ready!")
-
-    @client.event
-    async def on_message(message):
-        response = respond_to_command(message.content)
-        if response is not None:
-            await client.send_message(message.channel, response)
-
-    client.run(bot_token)
-
+    disco_bot = DiscoDiceBot("bot_credentials.txt")
+    disco_bot.run()
