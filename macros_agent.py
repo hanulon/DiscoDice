fcff88b1f008554b3fce6f990c4c4e7257c5b8f8
hanulon <enialis4@gmail.com>
2018-07-22 12:47:50 +0200

    Saving macros in memory.

--- macros_agent.py
+++ macros_agent.py
@@ -1,4 +1,18 @@
 
 class MacrosAgent:
-    def test(self):
-        return "test"
+    def __init__(self):
+        self.macros_store_filename = 'macros_store.txt'
+        self.macros_in_memory = []
+
+    def add_macro(self, commands, alias):
+        if alias is None or commands is None:
+            raise ValueError("Command/alias cannot be None")
+        if alias.__len__() == 0 or commands.__len__() == 0:
+            raise ValueError("Command/alias cannot be zero-length strings.")
+
+        new_macro = {'alias': alias, 'command': commands}
+
+        if new_macro['alias'] in [element['alias'] for element in self.macros_in_memory]:
+            raise ValueError("There already exists macro with that alias.")
+
+        self.macros_in_memory.append(new_macro)
--- tests/test_macros_agent.py
+++ tests/test_macros_agent.py
@@ -3,6 +3,39 @@ import macros_agent as macros
 
 
 class TestMacrosAgent(TestCase):
-    def test_test(self):
+    def test_add_macro_should_add_element_to_macro_list(self):
         agent = macros.MacrosAgent()
-        self.assertEqual(agent.test(), "test")
+        new_macro = ["stamina_roll", "/roll 4d10"]
+
+        agent.add_macro(new_macro[1], new_macro[0])
+
+        self.assertEqual(agent.macros_in_memory[0], {'alias': new_macro[0], 'command': new_macro[1]})
+
+    def test_add_macro_should_raise_exception_on_missing_alias_or_commands(self):
+        agent = macros.MacrosAgent()
+        new_macro = ["stamina_roll", "/roll 4d10"]
+
+        with self.assertRaises(expected_exception=ValueError):
+            agent.add_macro(None, new_macro[0])
+        with self.assertRaises(expected_exception=ValueError):
+            agent.add_macro("", new_macro[0])
+        with self.assertRaises(expected_exception=ValueError):
+            agent.add_macro(new_macro[1], "")
+
+    def test_add_macro_should_element_be_dictionary(self):
+        agent = macros.MacrosAgent()
+        new_macro = ["stamina_roll", "/roll 4d10"]
+
+        agent.add_macro(new_macro[1], new_macro[0])
+
+        self.assertEqual(agent.macros_in_memory[0]['alias'], "stamina_roll")
+        self.assertEqual(agent.macros_in_memory[0]['command'], "/roll 4d10")
+
+    def test_add_macro_should_raise_exception_on_duplicating_aliases(self):
+        agent = macros.MacrosAgent()
+        new_macro = ["stamina_roll", "/roll 4d10"]
+
+        agent.add_macro(new_macro[1], new_macro[0])
+
+        with self.assertRaises(expected_exception=ValueError):
+            agent.add_macro("whatever", new_macro[0])
